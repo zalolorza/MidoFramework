bbd9f98fd21b4e5523d9a18efd76d3610cfeba18
Zalo Lorza Planes <zalo@whitelabelcreative.co>
2018-08-08 06:18:24 -0400

    examples 2

--- examples/dws2018/postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    plugins: function () { // post css plugins, can be exported to postcss.config.js
-        return [
-          require('precss'),
-          require('autoprefixer')
-        ];
-      }
-  }
--- examples/mimcord2018/js/mixins/resize_throttled.js
+++
@@ -1,8 +0,0 @@
-var $ = require('jquery');
-var _ = require('underscore');
-
-var $window = $(window);
-
-$window.on('resize', _.throttle(function(){
-    $window.trigger('resizethrottled');
-}, 200));
